b4e7c78137c670b5042928d9df4a3be74c05cb6c
Troy Lamerton <troy.lamerton@gmail.com>
2017-01-03 15:53:21 +1300

    Add import from reddit url with error handling

--- firebase-angular/js/controllers/postCtrl.js
+++ firebase-angular/js/controllers/postCtrl.js
@@ -7,18 +7,19 @@
  * - exposes the model to the template and provides event handlers
  */
 postmvc.controller('PostCtrl', function PostCtrl($scope, $location, $firebaseArray) {
-	var url = 'https://data-filtering-tool.firebaseio.com/posts';
-	var fireRefMeta = new Firebase(url + '/meta')
-	var fireRefContent = new Firebase(url + '/content')
+	const url = 'https://data-filtering-tool.firebaseio.com/posts';
+	const fireRefMeta = new Firebase(url + '/meta')
+	const fireRefContent = new Firebase(url + '/content')
 
 	// Bind the posts to the firebase provider.
 	$scope.posts = $firebaseArray(fireRefMeta);
+	$scope.postsContent = $firebaseArray(fireRefContent);
 	$scope.newPostUrl = '';
 	$scope.editedPost = null;
 
 	$scope.$watch('posts', function () {
-		var total = 0;
-		var remaining = 0;
+		let total = 0;
+		let remaining = 0;
 		angular.forEach($scope.posts, function (post) {
 			// Skip invalid entries so they don't break the entire app.
 			if (!post || !post.title) {
@@ -36,36 +37,80 @@ postmvc.controller('PostCtrl', function PostCtrl($scope, $location, $firebaseArr
 		$scope.allChecked = remaining === 0;
 	}, true);
 
+	$scope.importUrlError = ({type} = {type: ''}) => {
+		switch (type) {
+			case 'DUPLICATE':
+				window.alert('Post is already in the list');
+				break;
+			default:
+				window.alert('URL is not a valid Reddit post url');
+		}
+	}
+
 	$scope.addPost = function () {
-		var newPostUrl = $scope.newPostUrl
+		let newPostUrl = $scope.newPostUrl.slice(0, -1)
 		if (!newPostUrl.length) {
 			return;
 		}
-		var urlParts = newPostUrl.split('/');
-		var id = urlParts[urlParts.length-3]
-		var title = urlParts[urlParts.length-2];
-
-		var xhr = new XMLHttpRequest();
-		if (self.fetch) {
-			console.log('fetch supported')
-		} else {
-			console.error('fetch supported')
-		}
-		fetch(newPostUrl)
-			.then(function(res) {
-				console.info(res);
-			})
-			.catch(function (err) {
-				console.error('Error fetching reddit data', err);
+		const urlParts = newPostUrl.split('/');
+		if (urlParts.length >= 6) {
+			// check if this post is already in the list
+			const newPostId = urlParts[urlParts.length-2]
+			const duplicatePost = $scope.posts.find(function(post) {
+				return post.id === newPostId;
 			});
+			if (duplicatePost) {
+				$scope.importUrlError({type: 'DUPLICATE'}); 
+				return;
+			}
+		}
+		else {
+			return $scope.importUrlError();
+		}
 
-		$scope.posts.$add({
-			id,
-			title,
-			url: newPostUrl,
-			marked: false
-		});
-		$scope.newPostUrl = '';
+		const queryStringStart = newPostUrl.indexOf('?');
+		if (queryStringStart > -1) {
+			newPostUrl = newPostUrl.slice(0, queryStringStart);
+		}
+		const redditUrlRegex = /https:\/\/(?:www\.)?reddit\.com\/[-a-zA-Z0-9@:%._\+~#=\/]{2,256}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/;
+		if (!self.fetch) {
+			console.error('JavaScript fetch NOT supported');
+			return;
+		}
+		if (redditUrlRegex.test(newPostUrl)) {
+			fetch(newPostUrl + '.json')
+				.then(function(res) {
+					res.json().then(function(data) {
+						if (typeof data === 'object' && data instanceof Array) {
+							const postData = data[0].data.children[0].data;
+							const { title, id, author, score, created_utc, url, selftext } = postData;
+							$scope.posts.$add({
+								id,
+								title,
+								author,
+								score,
+								url,
+								createdAt: created_utc,
+								marked: false,
+								hidden: false
+							});
+							fireRefContent.child(id).set({
+								id,
+								selftext
+							});
+							$scope.newPostUrl = '';
+						} else {
+							console.warn('Reddit data fetched json is not an Array')
+						}
+					})
+				})
+				.catch(function (err) {
+					console.error('Error fetching reddit data', err);
+				});
+		} else {
+			return $scope.importUrlError();
+		}
+
 	};
 
 	$scope.editPost = function (post) {
@@ -75,7 +120,7 @@ postmvc.controller('PostCtrl', function PostCtrl($scope, $location, $firebaseArr
 
 	$scope.doneEditing = function (post) {
 		$scope.editedPost = null;
-		var title = post.title;
+		const title = post.title;
 		if (title) {
 			$scope.posts.$save(post);
 		} else {
