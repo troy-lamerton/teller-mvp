e6e57fa3f1bdc7bfa8c87c8b50711d706943a11e
Troy Lamerton <troy.lamerton@gmail.com>
2017-01-22 12:04:18 +1300

    Clean up files for new project

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Teller MVP
+
+## Technical details
+
+Using AngularJS and Node, we will create a mock web application to showcase the Teller product.
+
+### Tools
+
+* Node
+* AngularJS
+  - Angular router
--- public/js/app.js
+++ public/js/app.js
@@ -7,29 +7,4 @@
  *
  * @type {angular.Module}
  */
-var postchooser = angular.module('postchooser', ['firebase']);
-
-postchooser.filter('postFilter', function ($location) {
-	return function (input) {
-		var filtered = {};
-		var path = $location.path();
-		angular.forEach(input, function (post, id) {
-			if (path === '/') {
-				if (post.marked) {
-					filtered[id] = post;
-				}
-			} else if (path === '/unfiled') {
-				if (!post.marked && !post.hidden) {
-					filtered[id] = post;
-				}
-			} else if (path === '/hidden') {
-				if (post.hidden) {
-					filtered[id] = post;
-				}
-			} else if (path === '/all') {
-				filtered[id] = post;
-			}
-		});
-		return filtered;
-	};
-});
+const teller = angular.module('teller', []);
