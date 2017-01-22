67a8c64a69d028aa21005d047ec9cbf4449acacd
Troy Lamerton <troy.lamerton@gmail.com>
2017-01-22 14:11:40 +1300

    Add angular routing

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Using AngularJS and Node, we will create a mock web application to showcase the 
 * Node
 * AngularJS
   - Angular router
+
+// "serve": "budo --dir ./index.html --dir ./public --live --watch-glob '**/*.{html,css,js}'",
--- public/app.js
+++ public/app.js
@@ -1,33 +1,38 @@
-	// create the module and name 'teller'
-	const teller = angular.module('teller', ['ngRoute']);
+// create the module and name 'app'
+var app = angular.module('teller', ['ngRoute']);
 
-	// configure our routes
-	teller.config(function($routeProvider) {
-		$routeProvider
-			.when('/', {
-				templateUrl : 'pages/home.html',
-				controller  : 'introController'
-			})
-			.when('/about', {
-				templateUrl : 'pages/about.html',
-				controller  : 'expensesController'
-			})
-			.when('/contact', {
-				templateUrl : 'pages/contact.html',
-				controller  : 'dataDisplayController'
-			});
-	});
+// configure our routes
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl : 'public/intro.html',
+			controller  : 'IntroCtrl'
+		})
+		.when('/expenses', {
+			templateUrl : 'public/expenses.html',
+			controller  : 'ExpensesCtrl'
+		})
+		.when('/result', {
+			templateUrl : 'public/dataDisplay.html',
+			controller  : 'DataDisplayCtrl'
+		});
+});
 
-	// create the controller and inject Angular's $scope
-	teller.controller('introController', function($scope) {
-		// create a message to display in our view
-		$scope.message = 'Welcome to Teller!';
-	});
+app.controller('MainCtrl', function($scope) {
+	// create a message to display in our view
+	$scope.message = 'Welcome to Teller!';
+});
 
-	teller.controller('expensesController', function($scope) {
-		$scope.message = 'Add your income and expesnes below';
-	});
+// create the controller and inject Angular's $scope
+app.controller('IntroCtrl', function($scope) {
+	// create a message to display in our view
+	$scope.message = 'Welcome to Teller on the intro page!';
+});
 
-	teller.controller('dataDisplayController', function($scope) {
-		$scope.message = 'Look at all the pretty data...';
-	});
+app.controller('ExpensesCtrl', function($scope) {
+	$scope.message = 'Add your income and expenses below';
+});
+
+app.controller('DataDisplayCtrl', function($scope) {
+	$scope.message = 'Look at all the pretty data...';
+});
